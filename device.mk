a29c5802a20ce9e5c11eea64d6c9436674a33bcf
Apavayan <info@apavayan.com>
2016-04-27 14:41:00 +0530

    j7elte: fix the typo in overrides props
    Change-Id: I2db85cf156fe94f2e8b53f7a34ac74acc020cf65
    Signed-off-by: Apavayan <info@apavayan.com>

--- device.mk
+++ device.mk
@@ -180,11 +180,11 @@ ADDITIONAL_DEFAULT_PROPERTIES += \
     dalvik.vm.isa.arm.features=div
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=8m
-    dalvik.vm.heapgrowthlimit=128m
-    dalvik.vm.heapsize=512m
-    dalvik.vm.heaptargetutilization=0.75
-    dalvik.vm.heapminfree=2m
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=128m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=2m \
     dalvik.vm.heapmaxfree=8m
 
 # call Samsung LSI board support package
